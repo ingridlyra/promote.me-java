ee2b3345d97976ba77faa8a341c65914893e80a6
ingridlyra <ingridmlbl@gmail.com>
2020-09-18 15:08:31 -0300

    refatorado o main

--- src/br/com/me/promote/Main.java
+++ src/br/com/me/promote/Main.java
@@ -30,17 +30,14 @@ public class Main {
 		}
 	}
 
-	public static void main(String[] args) {
-		Logar acesso;
-		Feedback feedback;
-		Usuario usuario;
-		
+	public static void main(String[] args) {	
 		System.out.println("*** Bem-vindx ao Promote.me \n ***");
-		//TODO: CHAMAR AS CLASSES PRINCIPAIS
 		
 		boolean isRunning = true;
 		int opcao;
 		Scanner sc1 = new Scanner(System.in);
+		
+		Usuario currentUser = new Usuario(1, null, 1, 1, null, null);
 		
 		while(isRunning) {
 			System.out.print("Digite o número da opção que você deseja: \n 1- Perfil \n 2- Feedback \n");
@@ -49,18 +46,23 @@ public class Main {
 			
 			if(opcao == 1) {
 				int uOpc;
-				System.out.print("Digite o número da opção que você deseja: \n 1- Visualizar seu perfil \n 2- Adicionar/remover skills \n 3 - Alterar senha \n");
+				System.out.print("Digite o número da opção que você deseja: \n 1- Visualizar seu perfil \n 2- Alterar senha \n");
 				uOpc = sc1.nextInt();
 				
 				
 				if(uOpc == 1) {
-					//exibir o perfil
+					System.out.print(currentUser.getNomeUsuario());
+					System.out.print(currentUser.getLogin());
+					System.out.print(currentUser.getCodigoVaga());
+					System.out.print(currentUser.getDtAdmissaoVaga());
+					
 				} else if (uOpc == 2) {
-					//Perguntar onde deseja add ou remover skills
-				} else if (uOpc == 3) {
-					//Alterar senha do usuário
-				} else {
-					System.err.print("Não há essa opção no sistema, tente novamente");
+					System.out.print("Digite uma nova senha (deve conter 4 dígitos e ser formada apenas por números) \n");
+					int newSenha = sc1.nextInt();
+					currentUser.setSenhaUsuario(newSenha);
+					
+				}  else {
+					System.err.print("Não há essa opção no sistema, tente novamente \n");
 				}
 				
 				
@@ -73,9 +75,12 @@ public class Main {
 				if(uOpc == 1) {
 					//Visualizar feedbacks recebidos
 				} else if (uOpc == 2) {
-					//Enviar um feedback
+					System.out.print("Digite o seu feedback para " // TODO: SUBSTITUIR AQUI C O USUARIO DE DESTINO
+							);
+					System.out.print("Digite aqui a sua nota \n");
+						//TODO:COLOCAR AQUI P ATRIBUIR AO USUARIO DE DESTINO
 				} else {
-					System.err.print("Não há essa opção no sistema, tente novamente");
+					System.err.print("Não há essa opção no sistema, tente novamente \n");
 				}
 				
 				
