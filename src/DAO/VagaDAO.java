45c1a0a30748fc0500109761554270cf8b934ee2
ingridlyra <ingridmlbl@gmail.com>
2020-09-18 15:09:40 -0300

    adc conexão de banco c classe vaga

--- src/DAO/VagaDAO.java
+++ src/DAO/VagaDAO.java
@@ -1,10 +1,101 @@
 package DAO;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.sql.ResultSet;
 import Utils.Connector;
 import br.com.me.promote.Vaga;
 
 public class VagaDAO {
+String table = "T_PM_VAGA";
+	
+	public Vaga getVaga(int cd_vaga) {		
+		try {
+			Connector connector = new Connector();
+			ResultSet result = connector.ExecuteQuery("SELECT * FROM " + table + " WHERE cd_vaga=" + cd_vaga);
+
+			while (result.next()) {
+				Integer cdVaga = result.getInt("cd_vaga");
+				String titulo = result.getString("nm_vaga");
+				Integer cd_carreira = result.getInt("cd_carreira");
+				
+				connector.Close();
+				return new Vaga(cdVaga, titulo, cd_carreira);
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public void CreateUsuario(Vaga vaga) {
+		String query = "INSERT INTO " + table + " (cd_vaga, nm_vaga, cd_carreira ) "
+	                   + "VALUES ("
+				       + vaga.getCdVaga()                            + ", "
+	                   + vaga.getCarreira()                    + ", "
+	                   + "'" + vaga.getTitulo() + "'"          + ", "
+	                   + ")";
+
+		try {
+			Connector connector = new Connector();
+			Boolean success = connector.ExecuteUpdate(query);
+			
+			if (success) {
+				System.out.println("Vaga salva!");
+			} else {
+				System.out.println("Algo deu errado");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Boolean UpdateVaga(Vaga newVaga) {
+		String query = "UPDATE " + table + " SET "
+				+ "cd_vaga=" + newVaga.getCdVaga()     +  ","
+				+ "cd_carreira=" + newVaga.getCarreira()  +  ","
+				+ "nm_vaga='" + newVaga.getTitulo() + "',"
+				+ "WHERE cd_vaga=" + newVaga.getCdVaga();
+
+		try {
+			Connector connector = new Connector();
+			Boolean success = connector.ExecuteUpdate(query);
+			
+			if (success) {
+				System.out.println("Vaga Atualizada!");
+				return true;
+			} else {
+				System.out.println("Algo deu errado");
+				return false;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public Boolean DeleteVaga(int vaga) {
+		String query = "DELETE FROM " + table + " WHERE cd_vaga=" + vaga;
+
+		try {
+			Connector connector = new Connector();
+			Boolean success = connector.ExecuteUpdate(query);
+			
+			if (success) {
+				System.out.println("Vaga Deletada!");
+				return true;
+			} else {
+				System.out.println("Algo deu errado");
+				return false;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
--- src/br/com/me/promote/Vaga.java
+++ src/br/com/me/promote/Vaga.java
@@ -1,40 +1,44 @@
 package br.com.me.promote;
 
-import java.util.ArrayList;
-
 public class Vaga {
+	private int cdVaga;
 	private String titulo;
-	private String descricao;
-	private ArrayList<String> requisitos = new ArrayList<String>();
+	private int carreira;
 	private double salario;
 	
+	public Vaga(int cdVaga, String titulo, int carreira) {
+	}
+
+	public int getCdVaga() {
+		return cdVaga;
+	}
+
+	public void setCdVaga(int cdVaga) {
+		this.cdVaga = cdVaga;
+	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-	public String getDescricao() {
-		return descricao;
+
+	public int getCarreira() {
+		return carreira;
 	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+
+	public void setCarreira(int carreira) {
+		this.carreira = carreira;
 	}
-	public ArrayList<String> getRequisitos() {
-		return requisitos;
-	}
-	public void setRequisitos(ArrayList<String> requisitos) {
-		this.requisitos = requisitos;
-	}
+
 	public double getSalario() {
 		return salario;
 	}
+
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	
-	
-	
-	//TODO: CRIAR OS METODOS DE VAGA
 
 }
