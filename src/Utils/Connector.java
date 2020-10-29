ba5bdaeb2ab7eded63ba507fb449cfef70bd0cc7
ingridlyra <ingridmlbl@gmail.com>
2020-10-29 12:34:49 -0300

    adc javadoc a classe do banco

--- src/Utils/Connector.java
+++ src/Utils/Connector.java
@@ -6,6 +6,11 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
 
+/* Essa classe é a classe de conexão com o banco de dados e ela só pode ser instanciada uma única vez (Singleton)
+ * @author Ingrid Lyra
+ * @since 1ª entrega do 2º semestre da challenge 2020
+ */
+
 public class Connector {
 	private Connection con;
 	private String oracleServer = "oracle.fiap.com.br";
@@ -14,6 +19,9 @@ public class Connector {
 	private String databasePass = "170100";
 	
 	private static Connector instancia = null;
+	
+	/** Método de controle de instâncias
+	 * @return instancia - uma instância, nula ou do objeto connector, que é a conexão com banco */
 	
 	public synchronized static Connector getInstancia() {
 		if(instancia == null) {
@@ -28,7 +36,7 @@ public class Connector {
 		return instancia;
 	}
 	
-	
+	/** Método de acesso ao banco*/	
 	public Connector() throws SQLException {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -38,7 +46,8 @@ public class Connector {
 			e.printStackTrace();
 		}		
 	}
-	
+	/** Método de execução de querys
+	 * @return null*/	
 	public ResultSet ExecuteQuery(String query) {
 		try {
 			Statement stmt = con.createStatement();
@@ -51,6 +60,8 @@ public class Connector {
 		return null;
 	}
 	
+	/** Método para executar um update do banco através do código
+	 * @return Boolean - se foi realizado o update ou não, se não houver nenhum erro, retorna true */
 	public Boolean ExecuteUpdate(String query) {
 		try {
 			Statement stmt = con.createStatement();
@@ -64,6 +75,7 @@ public class Connector {
 		return true;
 	}
 	
+	/** Método de controle de fechamento da conexão com o banco*/
 	public void Close() {
 		try {
 			con.close();
