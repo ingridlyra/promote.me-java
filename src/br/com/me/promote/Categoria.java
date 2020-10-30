a2bd63e381577617091d3c72d9fc7abddc6da288
ingridlyra <ingridmlbl@gmail.com>
2020-10-30 12:44:38 -0300

    adc javadoc

--- src/br/com/me/promote/Categoria.java
+++ src/br/com/me/promote/Categoria.java
@@ -1,5 +1,9 @@
 package br.com.me.promote;
 
+/* Esse é um enum que define à qual categoria (fixa, a princípio, para fins de desenvolvimento do sistema) um usuário do sistema pertence.
+ * @since 1ª entrega do 2º semestre da challenge 2020
+ */
+
 public enum Categoria {
 	adm, liderSetor, colaborador, liderRh, colabRh;
 	
