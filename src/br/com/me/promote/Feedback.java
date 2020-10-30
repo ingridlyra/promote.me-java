7cfaafa3dd3e94cccd4137c3c9463c7cfdbd6847
ingridlyra <ingridmlbl@gmail.com>
2020-10-30 12:42:38 -0300

    adc javadoc

--- src/br/com/me/promote/Feedback.java
+++ src/br/com/me/promote/Feedback.java
@@ -2,12 +2,24 @@ package br.com.me.promote;
 
 import java.time.LocalDate;
 
+/* Essa classe define um objeto do tipo Feedback.
+ * @since 1ª entrega do 2º semestre da challenge 2020
+ */
+
 public class Feedback {
 	int usuarioEnvio;
 	int usuarioReceptor;
 	String mensagem;
 	double nota;
 	LocalDate dataEnvio;
+	
+	public Feedback(int usuarioEnvio, int usuarioReceptor, String mensagem, double nota, LocalDate dataEnvio) {
+		this.usuarioEnvio = usuarioEnvio;
+		this.usuarioReceptor = usuarioReceptor;
+		this.mensagem = mensagem;
+		this.nota = nota;
+		this.dataEnvio = dataEnvio;
+	}
 	
 	public int getUsuarioEnvio() {
 		return usuarioEnvio;
@@ -48,16 +60,5 @@ public class Feedback {
 	public void setDataEnvio(LocalDate dataEnvio) {
 		this.dataEnvio = dataEnvio;
 	}
-
-	public Feedback(int usuarioEnvio, int usuarioReceptor, String mensagem, double nota, LocalDate dataEnvio) {
-		this.usuarioEnvio = usuarioEnvio;
-		this.usuarioReceptor = usuarioReceptor;
-		this.mensagem = mensagem;
-		this.nota = nota;
-		this.dataEnvio = dataEnvio;
-	}
-	
-	//TODO: aqui ficará os dados de feedback e o sistema que mostrará se foi enviado ou não, 
-	//assim como de notificar caso não tenha sido enviado
 
 }
